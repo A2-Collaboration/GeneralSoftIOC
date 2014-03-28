54d3014dc20d7f839562c6a9f10e60239ea95d17
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-03-28 15:16:35 +0100

    Adding tagging efficiency PV from pairspec

--- ioc/Run.sh
+++ ioc/Run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p /tmp/ioc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-softIoc -d general.db -d MWPC.db -d CB.db -d Scratch.db -d NMR.db dump.cmd
+softIoc -d general.db -d MWPC.db -d CB.db -d Scratch.db -d NMR.db -d PairSpec.db dump.cmd
